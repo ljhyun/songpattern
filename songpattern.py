79b8b2f14994a3c9d37cded53bd4f6c236535609
= <lejhyun@gmail.com>
2015-12-27 01:27:14 -0500

    updated init function

--- songpattern.py
+++ songpattern.py
@@ -4,12 +4,18 @@ import random
 import operator
 import sys
 
+'''
+Converts MARSYAS[1] lat,long class vector to specific country class
+Input: filein: MARSYAS encoded file name
+       fileout: output file name 
+'''
 def parseData(filein, fileout):
     geolocator = Nominatim()
     locfile = open(filein,'rb')
     wrtfile = open(fileout,'w')
     data = [line.strip().split(',') for line in locfile]
     for row in range(0,len(data)):
+        #Try to use the geolocator service with timeout of 5 seconds, if the country does not exist/timeout fill the country as 'nb'
         try:
             ct_code = geolocator.reverse(data[row][len(data[row])-2]+","+data[row][len(data[row])-1],timeout=5).raw
             if (ct_code != None and ct_code.get('address') != None):
@@ -26,10 +32,17 @@ def parseData(filein, fileout):
 def isposdef(num):
     return numpy.all(numpy.linalg.eigvals(num)>0)
 
+'''
+Initalization using knn method from [3]
+Input: feature: feature vector from MARSYAS[1] generated output 
+       centroid: list containing the index of the initial centers corresponding to the feature vector
+Output: Map value containing initial mean 1xC, weight 1xC, covariance CxXxX
+'''
 def findInitValue(feature, centroid):
     partition = [[] for l in range(0,len(centroid))]
+    #Covariance 
     covk =  [[[0]*len(feature[0]) for k in range(0,len(feature[0]))] for l in range(0,len(centroid))]
-    covk = covk.astype('float')
+    #Mean
     meank = [[0]*len(feature[0]) for l in range(0,len(centroid))]
 
     for f in range(0,len(feature)):
@@ -41,30 +54,33 @@ def findInitValue(feature, centroid):
                 kmin = mdist
                 kidx = kth
         partition[kidx].append(f)
-   
-    #TODO: catch division by zero exception
+    #Weight
     wgtk = [len(k)/len(feature) for k in partition]
-    a = [feature[item] for item in partition[0]]
     for c in range(0,len(partition)):
         for item in partition[c]:
             meank[c] = numpy.add(meank[c],feature[item])
 
-    #TODO: Check covariance matrix for positive semidefinite
+    #TODO: Add check for spherical covariance matrix ||x(n) - u(k)||2 * I
     meank = [map(operator.div,mean,[len(mean)]*len(mean)) for mean in meank]
     for cluster in range(0,len(centroid)):
         diff = [0 for l in range(0,len(partition[cluster]))]
-        for row in partition:
-            #print feature[row[cluster]]
-            #print "NEW"
-            #print meank[cluster]
-            diff = map(operator.sub,feature[row[cluster]],meank[cluster])
+        for index in partition[cluster]:
+            diff = map(operator.sub,feature[index],meank[cluster])
             covk[cluster] = numpy.add(covk[cluster],numpy.multiply(diff,numpy.matrix.transpose(numpy.matrix(diff))))
-            covk[cluster] = covk[cluster] / len(partition[cluster])
-            print covk[0]
+        covk[cluster] = numpy.divide(covk[cluster],float(len(partition[cluster])))
         if not isposdef(covk[cluster]):
             covk[cluster] = numpy.identity(len(feature[0]))
-            
+
     return {'mean':meank, 'covar':covk, 'weight':wgtk}
+
+'''
+Executes the EM algorithm and outputs the final parameters
+Input: init: Initialization Value from findInitValue
+       traindata: Training Data 
+Output: Map value containing initial mean 1xC, weight 1xC, covariance CxXxX
+'''
+def trainGMM(init,traindata):
+    return 0
 
 if __name__ == "__main__":
     newdata = './Geographical Original of Music-2/translate_default_features_1059_tracks.txt';
@@ -86,7 +102,7 @@ if __name__ == "__main__":
             c_list[country] = i
     feat = [[float(ele.replace('\'','')) for ele in row] for row in feat]
     #Initalize using kmeans
-    print findInitValue(feat,c_list.values())
-    
+    init = findInitValue(feat,c_list.values())
+        
 
     
